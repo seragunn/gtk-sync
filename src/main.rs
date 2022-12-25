52ec067996b50d702526a4b27caacd00087b6493
Trevor Gunn <tgunn@live.ca>
2022-12-24 23:58:40 -0500

    build GUI from bottom up

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,6 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    // Window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Gtk Sync")
-        .build();
-
-    window.set_size_request(APP_WIDTH, APP_HEIGHT);
-
-    // Box Model
-    let box_model = Box::new(Orientation::Vertical, 4);
-    window.set_child(Some(&box_model));
-
-    // Grid
-    let button_grid = Grid::builder()
-        .margin_start(40)
-        .margin_end(40)
-        .margin_top(20)
-        .margin_bottom(20)
-        .halign(Align::Center)
-        .valign(Align::Center)
-        .row_spacing(6)
-        .column_spacing(20)
-        .build();
-    box_model.append(&button_grid);
-
     // Buttons
     let button_forward = Button::builder().label("Forward").build();
     let button_backward = Button::builder().label("Backward").build();
@@ -45,18 +20,12 @@ fn build_ui(app: &Application) {
 
     let delete_option = CheckButton::builder().label("Delete").build();
 
-    button_grid.attach(&button_forward, 0, 0, 1, 1);
-    button_grid.attach(&button_backward, 1, 0, 1, 1);
-    button_grid.attach(&button_confirm, 2, 0, 1, 1);
-    button_grid.attach(&delete_option, 3, 0, 1, 1);
-
     // ListBox for rsync output
     let list_box = ListBox::new();
     let list_scroll = ScrolledWindow::builder()
         .propagate_natural_height(true)
         .child(&list_box)
         .build();
-    box_model.append(&list_scroll);
 
     // Actions
     button_forward.connect_clicked(glib::clone!(
@@ -83,5 +52,35 @@ fn build_ui(app: &Application) {
         }
     ));
 
+    // Grid
+    let button_grid = Grid::builder()
+        .margin_start(40)
+        .margin_end(40)
+        .margin_top(20)
+        .margin_bottom(20)
+        .halign(Align::Center)
+        .valign(Align::Center)
+        .row_spacing(4)
+        .column_spacing(20)
+        .build();
+
+    button_grid.attach(&button_forward, 0, 0, 1, 1);
+    button_grid.attach(&button_backward, 1, 0, 1, 1);
+    button_grid.attach(&button_confirm, 2, 0, 1, 1);
+    button_grid.attach(&delete_option, 3, 0, 1, 1);
+
+    // Box Model
+    let box_model = Box::new(Orientation::Vertical, 0);
+    box_model.append(&button_grid);
+    box_model.append(&list_scroll);
+
+    // Window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Gtk Sync")
+        .child(&box_model)
+        .build();
+
+    window.set_size_request(APP_WIDTH, APP_HEIGHT);
     window.present();
 }
